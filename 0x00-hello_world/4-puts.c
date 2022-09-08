4cbe88bf025039be7c5306e34b7ea50c9328e19e
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-08 14:49:57 -0700

    4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#<stdio.h>
+/**
+ *main - the entry point for the compiler
+ *@void: no variable is given 
+ *
+ * Return: the function return 0 
+ */
+ 
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
