062cf9fbad39c0b929e47f1f01b1e647fe60d92d
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-09 04:52:19 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
-/*
+/**
+ *main - entry point for programe
  *
  *
- *
- *
+ *Return: 0
  */
 int main(void)
 {
+	int i;
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar("%c", alphabet[i]);
+	}
+
 	return (0);
 }
