1b0a0d062e69e864858907351f7d9165bec4de37
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-08 22:27:25 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - prints text to screen
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	char Char;
+	int Int;
+	long int Long_Int;
+	long long int Long_Long_Int;
+	float Float;
+
+	printf("Size of a char: %zu byte(s)\n", sizeof(Char));
+	printf("Size of a int: %zu byte(s)\n", sizeof(Int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(Long_Int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(Long_Long_Int));
+	printf("Size of a float: %zu bytes\n", sizeof(Float));
+	return (0);
+}
