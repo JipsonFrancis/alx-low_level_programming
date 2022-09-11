c8d5ca60e702933260f86c577648f95ade587d97
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 12:17:46 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-/*
+/**
+ *main - entry point for programe
  *
  *
- *
- *
+ *Return: 0
  */
 int main(void)
 {
+	int i;
+	char a[] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+
+	for (i = 0; i <= 26; i++)
+	{
+		putchar(a[i]);
+	}
+
 	return (0);
 }
