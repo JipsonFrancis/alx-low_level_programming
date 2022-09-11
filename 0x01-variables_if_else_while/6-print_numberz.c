5aecea9ea8d0c0d24bf611a6e8894ec4dbc92c7a
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 13:17:11 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
  *main - entry point for programe
  *
@@ -11,7 +11,7 @@ int main(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		putchar((char)i);
+		write(2, i, 2);
 		if (i == 9)
 			putchar('\n');
 	}
