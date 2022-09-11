acfc2016dfd7d64740511003892bdc3e8c30ee6f
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 13:04:14 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-/*
+/**
+ *main - entry point for programe
  *
  *
- *
- *
+ *Return: 0
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+		if (i == 9)
+			putchar('\n');
+	}
+
 	return (0);
 }
