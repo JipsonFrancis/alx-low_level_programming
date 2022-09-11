9cbec1a438c81f5f85472d1872d3cef629b33c8f
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 12:08:05 -0700

    3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
 
 	for (i = 0; i <= 26; i++)
