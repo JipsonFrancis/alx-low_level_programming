17458ae2e53c32c5d9b6a7420839641f8b154c21
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 21:13:20 -0700

    mirror

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	int i;
-	char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+	char a[] = {'\n', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-	for (i = 26; i <= 0; i--)
+	for (i = 26; i >= 0; i--)
 	{
 		putchar(a[i]);
 	}
