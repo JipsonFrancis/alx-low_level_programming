5ce0c5722bf44c270114737e6101557036fe4b44
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 21:20:26 -0700

    hexd

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-/*
+/**
+ *main - entry point for programe
  *
  *
- *
- *
+ *Return: 0
  */
 int main(void)
 {
+	int i;
+	char a[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a',
+		'b', 'c', 'd', 'e', 'f', '\n'};
+
+	for (i = 0; i <= 16; i++)
+	{
+		putchar(a[i]);
+	}
+
 	return (0);
 }
