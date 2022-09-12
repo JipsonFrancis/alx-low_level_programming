67fdb63c88b11bfe2ded96e20f4de69621216645
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 21:04:11 -0700

    7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-/*
+/**
+ *main - entry point for programe
  *
  *
- *
- *
+ *Return: 0
  */
 int main(void)
 {
+	int i;
+	char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
+
+	for (i = 26; i <= 0; i--)
+	{
+		putchar(a[i]);
+	}
+
 	return (0);
 }
