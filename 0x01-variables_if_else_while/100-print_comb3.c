2ef105f0786b90d1cbd121699a55f08f19339ab9
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-12 06:31:55 -0700

    skll

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ *main - entry point for programe
+ *
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int Dascii[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (!(i >= j))
+			{
+				putchar(Dascii[i]);
+				putchar(Dascii[j]);
+				putchar(',');
+				putchar(' ');
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+
+	return (0);
+}
