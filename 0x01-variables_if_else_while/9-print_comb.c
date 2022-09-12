6d63a4e5a43ecf9354ca4dcd283b1422f3c945df
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-11 21:30:27 -0700

    combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-/*
+/**
+ *main - entry point for programe
  *
  *
- *
- *
+ *Return: 0
  */
 int main(void)
 {
+	int i;
+	char decimal[] = {'0','1','2','3','4','5','6','7','8','9'};
+	
+	for (i = 0; i < 10; i++)
+	{
+		putchar(decimal[i]);
+		if (i == 9)
+			putchar('\n');
+		putchar(',');
+	}
+
 	return (0);
 }
