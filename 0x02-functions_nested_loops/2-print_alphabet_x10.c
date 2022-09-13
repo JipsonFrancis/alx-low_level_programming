bfdd8ece32bb9fa4e56935fa12bd8ad806c4809c
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-13 11:00:47 -0700

    array

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,16 @@ void print_alphabet_x10(void)
 {
 	int i;
 	int j;
+	char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
 
 	for (j = 0 ; j <= 10; j++)
 	{
-		for (i = 97 ; i <= 122; i++)
+		for (i = 0 ; i <= 26; i++)
 		{
-			_putchar(i);
-			if (i == 122)
-				_putchar(10);
+			_putchar(a[i]);
+			if (i == 26)
+				_putchar('\n');
 		}
 	}
 }
