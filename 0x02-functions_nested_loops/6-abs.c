f0c64ec6ff3125a61bd8960d1447bf52ca9047c5
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-13 11:59:27 -0700

    abs val

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - find absolute value for number
+ *
+ * @n : the number to find abs val
+ *
+ *
+ * Return: on postive integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (n * -1);
+	return (n);
+}
