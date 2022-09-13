713760d2e50dbceef7ebee817343cfe293b2699d
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-13 11:07:37 -0700

    space

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,15 +12,13 @@ void print_alphabet_x10(void)
 	int i;
 	int j;
 	char a[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-		    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '\n'};
 
 	for (j = 0 ; j <= 10; j++)
 	{
 		for (i = 0 ; i <= 26; i++)
 		{
 			_putchar(a[i]);
-			if (i == 26)
-				_putchar('\n');
 		}
 	}
 }
