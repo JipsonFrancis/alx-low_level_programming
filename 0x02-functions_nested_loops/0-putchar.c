2815004436b85b05b8234bc417b8e47b87aee276
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-13 05:48:26 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar"
+/**
+ *
+ * print - writes the characters to stdout via _putchar
+ *
+ * Return: On success 0.
+ */
+int print()
+{
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
+	_putchar(10);
+	
+	return (0);
+}
