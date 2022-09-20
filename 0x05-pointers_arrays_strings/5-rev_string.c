0780d631fbe62b66c1d47d09fa1bb296b4014ba3
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-20 16:58:47 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,25 +1,5 @@
 #include "main.h"
 /**
- * _strlen - function that returns the length of a string
- *
- * @s : pointer to parameter that is passed
- *
- * Return: The length of a string
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	int a = 0;
-
-	for (i = 0; s[i]; i++)
-		a++;
-
-	return (a);
-}
-
-/**
  * rev_string -  reverses a string.
  *
  * @s : pointer to parameter that is passed
@@ -28,20 +8,20 @@ int _strlen(char *s)
  */
 void rev_string(char *s)
 {
-	int i;
+	int i = 0;
+	int j = 0;
+	int a = 0;
 
-	int a = _strlen(s);
+	for (i = 0; s[i]; i++)
+		a++;
 
-	int x[a];
-
-	for (i = 0; i < a; i++)
-		x[a - (i + 1)] = s[i];
-
-	for (i = 0; i < a; i++)
-		s[i] = x[i];
+	for (i = 0; i < (a / 2); i++)
+	{
+		j = a[i];
+		a[i] = a[a - i - 1];
+		a[n - i - 1] = j;
+	}
 
 	for (i = 0; s[i]; i++)
 		_putchar(s[i]);
-
-	_putchar(10);
 }
