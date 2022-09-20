9486db4fa67a027af206c75f73adb619542f1934
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-20 16:22:12 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
- * print_rev - prints a string, in reverse, followed by a new line
+ * rev_string -  reverses a string.
  *
  * @s : pointer to parameter that is passed
  *
  * Return : void
  */
-void print_rev(char *s)
+void rev_string(char *s)
 {
 	int i;
 
-	int a = 0;
+	int a = _strlen(s);
 
-	for (i = 0; s[i]; i++)
-		a++;
+	int x[a];
 
-	for (i = a - 1; i >= 0; i--)
-		_putchar(s[i]);
+	for (i = 0; i < a; i++)
+		x[a - (i + 1)] = s[i];
 
-	_putchar(10);
+	for (i = 0; i < a; i++)
+		s[i] = x[i];
+
+	_strlen(s);
 }
