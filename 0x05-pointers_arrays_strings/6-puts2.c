6f96732d486c65ccb2a4b0e54a4f613d4c0a4528
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-20 17:13:09 -0700

    ^

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string.
+ *
+ * @str : pointer to parameter that is passed
+ *
+ * Return : void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(s[i]);
+	}
+}
