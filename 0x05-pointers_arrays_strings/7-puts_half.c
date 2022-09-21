6bda00a1d625c00844368a30f1de764291cb9bb5
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-20 17:38:49 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *
+ * @str : pointer to parameter that is passed
+ *
+ * Return : void
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	int size = 0;
+
+	for (i = 0; str[i]; i++)
+		size++;
+
+	if (size % 2 == 0)
+	{
+		for (i = (size / 2); i < size; i++)
+			_putchar(str[i]);
+	}
+	else
+	{
+		for (i = ((size - 1) / 2); i < size; i++)
+			_putchar(str[i]);
+	}
+
+	_putchar(10);
+}
