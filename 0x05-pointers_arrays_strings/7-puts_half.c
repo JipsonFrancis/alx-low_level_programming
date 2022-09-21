af8a87bb0188184b1e86d34d13a93b79202403f3
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-20 17:53:05 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,12 +17,12 @@ void puts_half(char *str)
 
 	if (size % 2 == 0)
 	{
-		for (i = (size / 2); i < size; i++)
+		for (i = (size / 2); str[i]; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = ((size - 1) / 2); i < size; i++)
+		for (i = ((size - 1) / 2); str[i]; i++)
 			_putchar(str[i]);
 	}
 
