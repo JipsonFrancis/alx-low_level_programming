53c54e6b117819ec86b7b8b9dfb7a02e52e345f1
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-22 09:56:54 -0700

    5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Change all lowercase letters to uppercase
+ *
+ * @a: string
+ *
+ * Return: a
+ */
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (i = 0; a[i]; i++)
+	{
+		if (a[i] > 96 && a[i] < 123)
+			a[i] -= 32;
+	}
+
+	return (a);
+}
