47964be5fc8f584572c874331387e0173f735708
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-22 09:15:14 -0700

    3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings.
+ *
+ * @s1: para 1
+ *
+ * @s2: para 2
+ *
+ * Return: negative or positive
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s[i] - s2[i]);
+	}
+
+	return (0);
+}
