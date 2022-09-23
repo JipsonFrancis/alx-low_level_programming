84b97406af711d8a31992e7e76a9b4559cb015e0
JipsonFrancis <Francis.boy.jipson.jnr@gmail.com>
2022-09-23 09:59:50 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,13 +10,26 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i, a, aa;
 
-	for (i = 0; s1[i] || s2[i]; i++)
+	i = 0;
+	a = 0;
+	aa = 0;
+
+	while (s1[a] != '\0')
+		a++;
+	while (s2[aa] != '\0')
+		aa++;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			return (-1);
+			return (s1[i] - s2[i]);
+		i++;
 	}
+	if (a > aa)
+		return (s1[aa]);
+	else if (a < aa)
+		return (s2[a] * -1);
 
 	return (0);
 }
